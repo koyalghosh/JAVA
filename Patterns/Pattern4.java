ffbd30c5fadf230a1be92e2ca58892737c62c755
koyalghosh <80525025+koyalghosh@users.noreply.github.com>
2022-05-13 14:59:46 +0530

    Create Pattern4.java

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,22 @@
+/* This program will print the following output:- 
+                    1    
+                    21   
+                    321  
+                    4321 
+                    54321
+*/
+
+import java.util.Scanner;
+class Pattern4 {
+    public static void main(String args[]){
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of rows:- ");
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=i;j>=1;j--)
+                System.out.print(j);
+            System.out.println();
+        }
+    }    
+}
