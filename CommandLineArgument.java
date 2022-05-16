6a802cd45177bc9f0db7473918ce72d98a9e9239
koyalghosh <80525025+koyalghosh@users.noreply.github.com>
2022-05-16 20:43:52 +0530

    Create CommandLineArgument.java

---
+++ CommandLineArgument.java
@@ -0,0 +1,18 @@
+class CommandLineArgument{
+	public static void main(String args[]){ //args is an array of string which will convert every coming value to string then receive it
+		int sum=0;
+		for(int i=0;i<args.length;i++){//will take input in command prompt during run command(java CommandLineArgument)
+			sum+=Integer.parseInt(args[i]);//convert string to integer
+		}
+		System.out.print("Sum is " + sum);
+	}
+}
+
+/* In command prompt window
+
+D:\Coding\JAVA>javac CommandLineArgument.java
+
+D:\Coding\JAVA>java CommandLineArgument 10 20 30
+Sum is 60
+D:\Coding\JAVA>
+*/
